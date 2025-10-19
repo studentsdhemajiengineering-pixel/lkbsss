884d2ecb58370964d921130037722d34ea23d9c4
Students DhemajiEngineering <studentsdhemajiengineering@gmail.com>
2025-10-19 06:51:28 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9406841619-47899",
+  "appId": "1:735191398505:web:4f6b5021e1116ef69e8af8",
+  "apiKey": "AIzaSyCRRnnilVgdyAfpMDrVGnTnn90rUKzEI60",
+  "authDomain": "studio-9406841619-47899.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "735191398505"
+};
