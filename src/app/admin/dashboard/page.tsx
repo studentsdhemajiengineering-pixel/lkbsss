bc48548868f8b4f5901712212b7a8a37a09757ad
Students DhemajiEngineering <studentsdhemajiengineering@gmail.com>
2025-10-19 06:28:24 +0000

    admin dashborad shoud be like this

--- src/app/admin/dashboard/page.tsx
+++ src/app/admin/dashboard/page.tsx
@@ -1,3 +1,4 @@
+
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { BarChart, Users, Newspaper, GalleryHorizontal, PlusCircle } from "lucide-react";
